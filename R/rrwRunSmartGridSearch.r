6a0308eb654bb0d5c556822efeabb48a954397ee
DJC <dalejcohen@gmail.com>
2022-02-08 14:40:15 -0500

    added some new functions

--- R/rrwRunSmartGridSearch.r
+++ R/rrwRunSmartGridSearch.r
@@ -43,6 +43,7 @@ rrwRunSmartGridSearch <- function(data, rrwModelList, minN = 20, dataOverlapCol 
 
   #remove low n conditions
   df.raw$rt <- ifelse(df.raw$n < minN, NA, df.raw$rt)
+  df.raw$pHit <- ifelse(df.raw$n < minN, NA, df.raw$pHit)
   #output the rrwData to a file
   filename <- paste(fileTag,"rrwData.txt")
   write.table(df.raw, filename, col.names=T, row.names=F, quote=F, sep="\t")
